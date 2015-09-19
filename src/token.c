1c9909a51e1ad84927bc737327aa52073b14fb82
Quentin Glidic <sardemff7+git@sardemff7.net>
2015-09-19 11:56:17 +0200

    Define our own G_LOG_DOMAIN
    Signed-off-by: Quentin Glidic <sardemff7+git@sardemff7.net>

--- src/token.c
+++ src/token.c
@@ -27,6 +27,11 @@
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
 
+#ifdef G_LOG_DOMAIN
+#undef G_LOG_DOMAIN
+#endif /* G_LOG_DOMAIN */
+#define G_LOG_DOMAIN "libnkutils-token"
+
 #include <string.h>
 
 #include <glib.h>
